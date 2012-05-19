0dd43b32bfdbedcf85bbeb348a73988372076975
Andrew Eberbach <andrew@ebertech.ca>
2012-05-19 14:56:13 -0400

    added config method

--- app/commands/outsourced/commands/worker/config.rb
+++ app/commands/outsourced/commands/worker/config.rb
@@ -1,5 +1,9 @@
 class Outsourced::Commands::Worker::Config < Outsourced::Commands::Worker::Command
-  def execute
+  requires_worker
 
+  def execute
+    with_worker(worker_name) do |worker|
+      puts worker.to_yaml
+    end
   end
 end
